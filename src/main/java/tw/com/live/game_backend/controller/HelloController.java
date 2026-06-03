d0e05ed6d8b85e02d896dfa9fb7336ee536727da
Hamster <ru81o650106@gmail.com>
2026-06-03 15:50:56 +0800

    新增 member API

--- src/main/java/tw/com/live/game_backend/controller/HelloController.java
+++ src/main/java/tw/com/live/game_backend/controller/HelloController.java
@@ -1,18 +1,46 @@
 package tw.com.live.game_backend.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@GetMapping("/")
-	public String hello() {
-		return "Hello Spring Boot";
+	@GetMapping("/streams")
+	public List<Map<String,Object>> streams(){
+		  List<Map<String, Object>> list = new ArrayList<>();
+		  
+		  Map<String, Object> stream1 = new HashMap<>();
+		    stream1.put("id", 1);
+		    stream1.put("title", "Minecraft 生存");
+		    stream1.put("streamer", "Hamster");
+
+		    Map<String, Object> stream2 = new HashMap<>();
+		    stream2.put("id", 2);
+		    stream2.put("title", "星海爭霸 II");
+		    stream2.put("streamer", "Raynor");
+
+		    list.add(stream1);
+		    list.add(stream2);
+
+		    return list;
 	}
 	
-	@GetMapping("/hamster")
-	public String hamster() {
-		return "倉鼠萬歲!";
+	@GetMapping("/member")
+	public  Map<String,Object> member() {;
+		
+		Map<String,Object> data = new HashMap<>();
+	
+	    data.put("id", 1);
+	    data.put("name", "Hamster");
+	    data.put("job", "Backend Student");
+	    
+	    return data;
 	}
+	
 }
